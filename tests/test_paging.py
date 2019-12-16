e26df82e1fd1412f3da785bd6fe3e7060f49f88a
Anthony Carapetis <anthony.carapetis@gmail.com>
2019-12-16 18:05:44 +1100

    Add tests for subquery and CTE compatibility

--- tests/test_paging.py
+++ tests/test_paging.py
@@ -3,7 +3,7 @@ import warnings
 import pytest
 from sqlalchemy import select, String, Column, Integer, ForeignKey, column, table, desc, func
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, aliased
 from sqlbag import temporary_database, S
 
 from sqlakeyset import get_page, select_page, serialize_bookmark, unserialize_bookmark, OC, process_args
@@ -26,6 +26,9 @@ class Book(Base):
     c = Column(Integer)
     d = Column(Integer)
     author_id = Column(Integer, ForeignKey('author.id'))
+    prequel_id = Column(Integer, ForeignKey(id), nullable=True)
+    prequel = relationship('Book', remote_side=[id],
+                           backref='sequel', uselist=False)
 
 
 class Author(Base):
@@ -35,8 +38,7 @@ class Author(Base):
     books = relationship('Book', backref='author')
 
 
-@pytest.fixture(params=['postgresql', 'mysql'])
-def dburl(request):
+def _dburl(request):
     count = 10
     data = []
 
@@ -51,9 +53,13 @@ def dburl(request):
 
     for x in range(count):
         author = Author(name='Author {}'.format(x))
+        abooks = []
         for y in range((2*x) % 10):
             b = Book(name='Book {}-{}'.format(x, y), a=x+y, b=(y*x) % 2, c=count - x, d=99-y)
             b.author = author
+            if y % 4 != 0:
+                b.prequel = abooks[(2*y+1) % len(abooks)]
+            abooks.append(b)
             data.append(b)
 
     with temporary_database(request.param) as dburl:
@@ -62,6 +68,8 @@ def dburl(request):
             s.add_all(data)
         yield dburl
 
+dburl = pytest.fixture(params=['postgresql', 'mysql'])(_dburl)
+pg_only_dburl = pytest.fixture(params=['postgresql'])(_dburl)
 
 def check_paging_orm(q):
     item_counts = range(1, 12)
@@ -162,13 +170,63 @@ def test_orm_query4(dburl):
         q = s.query(Book).order_by(Book.name)
         check_paging_orm(q=q)
 
-def test_orm_query5(dburl):
+
+def test_orm_query_aggregated(dburl):
     count = func.count().label('count')
     spec = [desc(count), desc(Author.name), Author.id]
 
     with S(dburl, echo=ECHO) as s:
         q = s.query(Author, count).join(Author.books) \
             .group_by(Author).order_by(*spec)
+        check_paging_orm(q=q)
+
+
+def test_orm_query_with_entities(dburl):
+    spec = [Author.name, Book.name, desc(Book.id)]
+
+    with S(dburl, echo=ECHO) as s:
+        q = s.query(Book).join(Book.author) \
+            .filter(Author.name.contains('1')
+                    | Author.name.contains('2')) \
+            .with_entities(Book.name, Author.name, Book.id) \
+            .order_by(*spec)
+        check_paging_orm(q=q)
+
+
+def test_orm_query_subquery(dburl):
+    count = func.count().label('count')
+
+    with S(dburl, echo=ECHO) as s:
+        sq = s.query(Author.id, count).join(Author.books) \
+            .group_by(Author.id).subquery('sq')
+        q = s.query(Author).join(sq, sq.c.id == Author.id) \
+            .with_entities(sq.c.count, Author) \
+            .order_by(desc(sq.c.count), Author.name, Author.id)
+        check_paging_orm(q=q)
+
+
+def test_orm_query_recursive_cte(pg_only_dburl):
+    with S(pg_only_dburl, echo=ECHO) as s:
+        seed = s.query(Book.id.label('id'), Book.id.label('origin')) \
+            .filter(Book.prequel == None)
+
+        # Recurse by picking up sequels
+        sequel = aliased(Book, name='sequel')
+        recursive = seed.cte(recursive=True)
+        recursive = recursive.union(
+            s.query(sequel.id, recursive.c.origin)
+            .filter(sequel.prequel_id == recursive.c.id)
+        )
+
+        # Aggregate and join
+        count = func.count().label('count')
+        origin = recursive.c.origin.label('origin')
+        sq = s.query(origin, count).group_by(origin).cte(recursive=False)
+
+        q = s.query(sq.c.count, Book) \
+            .filter(Book.id == sq.c.origin) \
+            .order_by(sq.c.count.desc(), Book.id)
+
         check_paging_orm(q=q)
 
 
