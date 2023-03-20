5a001e161d38c1d729c559946636019e049ca2b8
Anthony Carapetis <anthony.carapetis@gmail.com>
2023-03-20 14:16:17 +1100

    Reorganize SQLAlchemy version-dependent code

--- sqlakeyset/sqla.py
+++ sqlakeyset/sqla.py
@@ -15,8 +15,47 @@ def get_bind(q, s):
 
 
 if SQLA_VERSION < version.parse("1.4.0b1"):
-    from .sqla13 import *  # noqa
+    from .sqla13 import (
+        core_coerce_row,
+        core_result_type,
+        group_by_clauses,
+        order_by_clauses,
+        orm_coerce_row,
+        orm_query_keys,
+        orm_result_type,
+        orm_to_selectable,
+    )
 elif SQLA_VERSION < version.parse("2.0"):
-    from .sqla14 import *  # noqa
+    from .sqla14 import (
+        core_coerce_row,
+        core_result_type,
+        group_by_clauses,
+        order_by_clauses,
+        orm_coerce_row,
+        orm_query_keys,
+        orm_result_type,
+        orm_to_selectable,
+    )
 else:
-    from .sqla20 import *  # noqa
+    from .sqla20 import (
+        core_coerce_row,
+        core_result_type,
+        group_by_clauses,
+        order_by_clauses,
+        orm_coerce_row,
+        orm_query_keys,
+        orm_result_type,
+        orm_to_selectable,
+    )
+
+__all__ = [
+    "core_coerce_row",
+    "core_result_type",
+    "get_bind",
+    "group_by_clauses",
+    "order_by_clauses",
+    "orm_coerce_row",
+    "orm_query_keys",
+    "orm_result_type",
+    "orm_to_selectable",
+]
