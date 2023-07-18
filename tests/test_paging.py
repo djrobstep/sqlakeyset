8ecb334a6aba9b525f6a0a4b6f379b8ebadc751f
Matthew Albrecht <matt@delfina.com>
2023-07-18 13:02:12 -0700

    flake8.

--- tests/test_paging.py
+++ tests/test_paging.py
@@ -534,7 +534,7 @@ def test_core_multiple_pages_one_query(no_sqlite_dburl):
         check_multiple_paging_core(qs=qs, s=s)
 
 
-def test_core_multiple_pages_empty_queries():
+def test_core_multiple_pages_empty_queries(no_sqlite_dburl):
     with S(no_sqlite_dburl, echo=ECHO) as s:
         assert select_homogeneous_pages([], s) == []
 
